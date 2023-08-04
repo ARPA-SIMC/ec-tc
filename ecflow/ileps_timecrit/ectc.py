788cd4b5c177f5090c37200e63777daa738ec54b
Davide Cesari <dcesari@arpae.it>
2023-08-04 17:11:35 +0200

    First prototype of an ileps suite

---
+++ ecflow/ileps_timecrit/ectc.py
@@ -0,0 +1,215 @@
+#!/usr/bin/python
+
+import ecflow
+import os
+import datetime
+
+# some utility routines:
+
+# from https://rosettacode.org/wiki/Range_expansion%23Python&sa=D&ust=1517307153586000&usg=AFQjCNHTzzU1Kk2XfryWWIoWwKTWNWItiw
+def rangeexpand(txt):
+    lst = []
+    for r in txt.split(','):
+        if '-' in r[1:]:
+            r0, r1 = r[1:].split('-', 1)
+            lst += range(int(r[0] + r0), int(r1) + 1)
+        else:
+            lst.append(int(r))
+    return lst
+
+# incremental or for trigger syntax
+def expr_or(expr, orexpr):
+    if expr == "":
+        return orexpr
+    elif orexpr == "":
+        return expr
+    else:
+        return expr+" || "+orexpr
+
+
+def ask_confirm(msg=""):
+    ans = input(msg+" (y/n)? ")
+    return ans.startswith("y")
+
+
+def daily_cron(step):
+    if step > 60 or step < 0:
+        return None
+    time_series = ecflow.TimeSeries(ecflow.TimeSlot(0, 0),
+                                    ecflow.TimeSlot(23, 60-step),
+                                    ecflow.TimeSlot(0,step), False)
+    cron = ecflow.Cron()
+    cron.set_time_series(time_series)
+    return cron
+
+
+def cron_set_time(time):
+    cron = ecflow.Cron()
+    cron.set_time_series(time)
+    return cron
+
+
+# convert a member number to a member name for naming families
+def membname(eps_memb, memb_pref=""):
+    if eps_memb == -1:
+        return "control" # control
+    elif eps_memb == 0:
+        return "deterministic" # deterministic
+    else:
+        return f"{memb_pref}{eps_memb}"
+
+
+# class for creating and managing a suite (check, write to a def file,
+# update on ecflow server)
+class TcSuite():
+    def __init__(self, name):
+        self.name = name
+        self.defs = ecflow.Defs()
+        self.suite = self.defs.add_suite(name)
+        self.checked = False
+
+
+    def check(self):
+        # check syntax
+        result = self.defs.check()
+        if result != "":
+            print("Error in "+self.name+" suite definition:")
+            print(result)
+        else:
+            # check job tree
+            result = self.defs.check_job_creation()
+            if result != "":
+                print("Error in "+self.name+" suite job creation:")
+                print(result)
+            else: 
+                self.checked = True
+
+
+    def write(self, interactive=True):
+        if not self.checked:
+            print("suite "+self.name+" has not been checked, refusing to write")
+            return
+        name = self.name+".def"
+        if interactive:
+            if os.path.exists(name):
+                if not ask_confirm("Definition file "+name+" exists, replace"):
+                    return
+        self.defs.save_as_defs(name)
+        print("Suite saved in "+name)
+
+
+    def replace(self, interactive=True):
+        if not self.checked:
+            print("suite "+self.name+" has not been checked, refusing to replace")
+            return
+        if interactive:
+            if not ask_confirm("Replace suite "+self.name+" on server"):
+                return
+        client = ecflow.Client() # connect using environment
+        client.replace("/"+self.name, self.defs)
+        print("Suite "+self.name+" replaced on server")
+
+
+# base class for adding a family to a suite node, for not repeating
+# common code
+class TcFamily:
+    def __init__(self, conf={}):
+        self.conf = {}
+        self.conf.update(conf)
+
+
+# Add the root family for looping on days and hours (loop on hours not
+# implemented yet)
+class TcSuiteTime(TcFamily):
+    def add_to(self, node):
+        day = node.add_family("day").add_repeat(
+            ecflow.RepeatDate("YMD",
+                              int((datetime.datetime.now()-datetime.timedelta(days=self.conf["deltaday"])).strftime("%Y%m%d")),
+                              int((datetime.datetime.now()+datetime.timedelta(days=5000)).strftime("%Y%m%d"))))
+        return day # day is the new root of the suite. it must be returned
+
+
+# Add a family for starting suite 
+class TcStartSuite(TcFamily):
+    def add_to(self, node):
+        for sub in self.conf["subsuites"]: # ana, eps, det, 
+            fam = node.add_family("start_suite_"+sub)
+            fam.add_task("startileps_tc_"+sub)
+            fam.add_variable("ECF_DUMMY_TASK", "Y")
+
+
+# Add a pre family (data retrieval for cluster analysis, cluster
+# analysis, data retrieval for each ensemble member), data retrieval
+# can be in different flavours (mars, dissemination...)
+class TcPre(TcFamily):
+    def add_to(self, node):
+        for pre in self.conf["pretypes"]: # mars, diss
+            fam = node.add_family(f"pre_{pre}")
+            fam.add_late(ecflow.Late(active="09:30", complete="11:59")) # shift 00 or 12?
+            if self.conf["predefault"] != pre: fam.add_defstatus(ecflow.Defstatus("complete"))
+            fam.add_trigger("./start_suite_eps == complete")
+            fam.add_task(f"get_pl_{pre}")
+            fam.add_task("cluster_analysis").add_trigger(f"./get_pl_{pre} == complete")
+            fam.add_task("configure").add_trigger("./cluster_analysis == complete")
+            get = fam.add_family("get_ml") # inlimit /ileps_timecrit:get_ml_limit
+            for eps_memb in self.conf["membrange"]:
+                fname = membname(eps_memb, "eps_member_")
+                if eps_memb > 0: # eps membs depend on cluster analysis, det does not
+                    trig = "../../configure == complete" # check
+                elif eps_memb == 0:
+                    trig = "../../../start_suite_det == complete" # check
+                get.add_family(fname).add_task(f"retrieve_ec_bc_{pre}").add_trigger(trig).add_variable("ECF_ENS_MEMB", str(eps_memb))
+
+# Add a run family for every ensemble member
+class TcRun(TcFamily):
+    def add_to(self, node):
+        fam = node.add_family("iconrun") # was "lokal", task setup_lokal_det deleted!
+        for eps_memb in self.conf["membrange"]:
+            fname = membname(eps_memb, "eps_member_")
+            run = fam.add_family(fname)
+            run.add_variable("ECF_ENS_MEMB", str(eps_memb))
+            trig = ""
+            for pre in self.conf["pretypes"]: # mars, diss
+                trig = expr_or(trig, f"../../pre_{pre}/get_ml/{fname} == complete")
+            run.add_task("remap").add_trigger(trig)
+            run.add_task("icon").add_trigger("./icon == complete")#  && ../../icon2leps == complete") # was icon2leps/icon2leps (task)
+
+
+
+if __name__ == '__main__':
+    interactive = True
+    # vars and configuration management to be improved    
+    ecf_vars={"SCHOST": "hpc-login",
+              "ECF_FILES": os.path.join(os.getcwd(), "ecffiles"),
+              "ECF_INCLUDE": os.path.join(os.getcwd(), "include"),
+              "ECF_HOME": os.path.join(os.getcwd(), "work"),
+              "ECF_STATUS_CMD": "/opt/troika/bin/troika monitor %SCHOST% %ECF_JOB%",
+              "ECF_KILL_CMD": "/opt/troika/bin/troika kill %SCHOST% %ECF_JOB%",
+              "ECF_JOB_CMD": "/opt/troika/bin/troika submit -o %ECF_JOBOUT% %SCHOST% %ECF_JOB%",
+              "ECF_TRIES": "2",
+    }
+    conf={"deltaday": 1,
+          "hours": range(0, 24, 12), # (0,24,24) to run only at 00
+          "subsuites": ("ana", "eps", "det"),
+          "pretypes": ("mars", "diss"),
+          "predefault": "mars",
+          "membrange": range(5)}
+
+    # create a suite, ileps.suite will be the root node of the suite
+    ileps = TcSuite("ileps_timecrit")
+    # add configure ECF vars, this should be moved to a special subroutine
+    for var in ecf_vars:
+        ileps.suite.add_variable(var, ecf_vars[var])
+    # add timing loop
+    newroot = TcSuiteTime(conf).add_to(ileps.suite)
+    # add the rest of the suite elements to the new root of the suite
+    TcStartSuite(conf).add_to(newroot) # short for f=TcStartSuite(conf); f.add_to(newroot)
+    TcPre(conf).add_to(newroot)
+    TcRun(conf).add_to(newroot)
+    # check the suite (syntax, triggers, jobs)
+    ileps.check()
+    #ileps.checked=True
+    # write suite to a .def file
+    ileps.write(interactive=interactive)
+    # upload/replace on server (with confirmation if interactive)
+    #ileps.replace(interactive=interactive)
